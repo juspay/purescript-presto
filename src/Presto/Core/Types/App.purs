9782842857710eaee3a5e3188f2e0fd43273cfab
ashutoshdas96 <ashutosh.das@juspay.in>
2018-05-14 22:25:30 +0530

    added REF to AppEffects

--- src/Presto/Core/Types/App.purs
+++ src/Presto/Core/Types/App.purs
@@ -13,6 +13,7 @@ import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Aff.Console (CONSOLE)
 import Control.Monad.Eff (kind Effect)
 import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Eff.Ref (REF)
 import DOM (DOM)
 import FRP (FRP)
 
@@ -21,6 +22,18 @@ foreign import data STORAGE :: Effect
 foreign import data NETWORK :: Effect
 foreign import data UI :: Effect
 
-type AppEffects eff = (avar :: AVAR, frp :: FRP, dom :: DOM, ui :: UI, storage :: STORAGE, ls :: LOCAL_STORAGE, exception :: EXCEPTION, network :: NETWORK, console :: CONSOLE | eff)
+type AppEffects eff =
+    ( avar :: AVAR
+    , ref :: REF
+    , frp :: FRP
+    , dom :: DOM
+    , ui :: UI
+    , storage :: STORAGE
+    , ls :: LOCAL_STORAGE
+    , exception :: EXCEPTION
+    , network :: NETWORK
+    , console :: CONSOLE
+    | eff
+    )
 type AppFlow eff = Aff (AppEffects eff)
-type UIFlow eff = Aff (frp :: FRP, dom :: DOM | eff)
+type UIFlow eff = Aff (ref :: REF, frp :: FRP, dom :: DOM | eff)
