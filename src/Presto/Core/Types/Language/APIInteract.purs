26de4285e3561066b9d758dea3f2787236da9eb5
Aman Kasliwal <aman.kasliwal@juspay.in>
2018-06-05 19:21:55 +0530

    cleaner implementation of api interact fn

--- src/Presto/Core/Types/Language/APIInteract.purs
+++ src/Presto/Core/Types/Language/APIInteract.purs
@@ -5,11 +5,12 @@ module Presto.Core.Types.Language.APIInteract
 import Prelude
 
 import Control.Monad.Except (runExcept)
-import Data.Either (Either(..))
+import Data.Bifunctor (bimap)
+import Data.Either (Either, either)
+import Data.Foreign (Foreign)
 import Data.Foreign.Class (class Decode, class Encode, decode, encode)
-
-import Presto.Core.Types.Language.Interaction (Interaction, request)
 import Presto.Core.Types.API (class RestEndpoint, ErrorPayload(..), ErrorResponse, Response(..), Headers, decodeResponse, makeRequest)
+import Presto.Core.Types.Language.Interaction (Interaction, request)
 import Presto.Core.Utils.Encoding (defaultDecodeJSON)
 
 -- Special interact function for API.
@@ -18,16 +19,18 @@ apiInteract :: forall a b.
   => a -> Headers -> Interaction (Either ErrorResponse b)
 apiInteract a headers = do
   fgnOut <- request (encode (makeRequest a headers))
-  pure $ case runExcept (decode fgnOut >>= decodeResponse) of
-    -- Try to decode the server's resopnse into the expected type
-    Right resp -> Right resp
-    Left x -> Left $ case runExcept (decode fgnOut >>= defaultDecodeJSON) of
-                       -- See if the server sent an error response, else create our own
-                       Right e@(Response _) -> e
-                       Left y -> Response { code: 0
-                                          , status: ""
-                                          , response: ErrorPayload { error: true
-                                                                   , errorMessage: show x <> "\n" <> show y
-                                                                   , userMessage: "Unknown error"
-                                                                   }
-                                          }
+  pure $ bimap (decodeErrorResponse fgnOut) id (runExcept (decode fgnOut >>= decodeResponse))
+
+-- See if the server sent an error response, else create our own
+decodeErrorResponse :: forall a. Show a => Foreign -> a -> Response ErrorPayload
+decodeErrorResponse fgnOut x = 
+  either (defaultErrorResponse x) id (runExcept (decode fgnOut >>= defaultDecodeJSON))
+
+defaultErrorResponse :: forall a b. Show a => Show b => a -> b -> Response ErrorPayload
+defaultErrorResponse x y = Response { code: 0
+                      , status: ""
+                      , response: ErrorPayload { error: true
+                                                , errorMessage: show x <> "\n" <> show y
+                                                , userMessage: "Unknown error"
+                                                }
+                      }
