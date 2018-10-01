f5c043428522855683efe94c3e989a63c1cce8aa
Dilip Jain <dilip.jain@juspay.in>
2018-10-01 16:27:58 +0530

    Counter : Updating presto to v0.4

--- examples/counter-html/src/Types.purs
+++ examples/counter-html/src/Types.purs
@@ -2,33 +2,17 @@ module Types where
 
 import Prelude
 
-import Control.Monad.Aff (Aff)
-import Control.Monad.Aff.AVar (AVAR)
-import Control.Monad.Eff (Eff, kind Effect)
-import Control.Monad.Eff.Console (CONSOLE)
-import Control.Monad.Eff.Exception (EXCEPTION, Error)
+import Effect.Aff (Aff)
 import Presto.Core.Types.API (URL)
-import Presto.Core.Types.App (LOCAL_STORAGE, NETWORK, STORAGE, UI)
-
-foreign import data TIMER :: Effect
+import Effect (Effect)
+import Effect.Exception (Error)
 
 type NativeHeader = { field :: String , value :: String}
-type AffStorage e = Aff (storage :: STORAGE | e)
-type EffStorage e = Eff (storage :: STORAGE | e)
-type AffError e = (Error -> Eff e Unit)
-type AffSuccess s e = (s -> Eff e Unit)
+type AffStorage = Aff
+type EffStorage = Effect
+type AffError = (Error -> Effect Unit)
+type AffSuccess s = (s -> Effect Unit)
 type NativeHeaders = Array NativeHeader
-
-type AppEffects = (
-    avar :: AVAR
-  , exception :: EXCEPTION
-  , exception :: EXCEPTION
-  , ui :: UI
-  , storage :: STORAGE
-  , ls :: LOCAL_STORAGE
-  , console :: CONSOLE
-  , network :: NETWORK
-  , timer :: TIMER)
 
 newtype NativeRequest = NativeRequest
  { method :: String
