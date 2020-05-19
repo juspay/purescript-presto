2eeeed732f4b261dc40ab0b8c6ccc6f76e67c1ff
Nandini <nandini.singh@juspay.in>
2020-05-19 09:27:52 +0530

    another api interact

--- src/Presto/Core/Types/Language/APIInteract.purs
+++ src/Presto/Core/Types/Language/APIInteract.purs
@@ -12,19 +12,33 @@ import Presto.Core.Types.Language.Interaction (Interaction, request)
 import Presto.Core.Types.API (class RestEndpoint, ErrorPayload(..), ErrorResponse, Response(..), Headers, decodeResponse, makeRequest)
 import Presto.Core.Utils.Encoding (defaultDecodeJSON)
 
+import Tracker (trackApiCall, trackException) as Tracker
+import Tracker.Labels (Label(..)) as Tracker
+import Tracker.Types (Level(..), Subcategory(..), Category(API_CALL)) as Tracker
+
+import Presto.Core.Language.Runtime.API(getTime)
+
 -- Special interact function for API.
 apiInteract :: forall a b.
   Encode a => Decode b => RestEndpoint a b
   => a -> Headers -> Interaction (Either ErrorResponse b)
 apiInteract a headers = do
+  startTime <- getTime
   fgnOut <- request (encode (makeRequest a headers))
   pure $ case runExcept (decode fgnOut >>= decodeResponse) of
     -- Try to decode the server's resopnse into the expected type
-    Right resp -> Right resp
-    Left x -> Left $ case runExcept (decode fgnOut >>= defaultDecodeJSON) of
+    Right resp -> do
+      _ <- Tracker.trackApiCall Tracker.Sdk Tracker.Info Tracker.DETAILS startTime getTime resp.status (encode resp) r.url r.payload
+      Right resp
+    Left x -> do
+      Left $ case runExcept (decode fgnOut >>= defaultDecodeJSON) of
                        -- See if the server sent an error response, else create our own
-                       Right e@(Response _) -> e
-                       Left y -> Response
+                       Right e@(Response _) -> do
+                        _ <- Tracker.trackException Tracker.API_CALL Tracker.Sdk Tracker.Error Tracker.DECODE_ERROR (show e)
+                        e
+                       Left y -> do
+                        _ <- Tracker.trackException Tracker.API_CALL Tracker.Sdk Tracker.Error Tracker.DECODE_ERROR ("Unknown error")
+                        Response
                                     { code : 0
                                     , status : ""
                                     , response : ErrorPayload
